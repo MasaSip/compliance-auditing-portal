22b82d54c199a61c02e0bfaddc6e0da7a27d7907
Stefan Guelland <stefan@sguelland.de>
2018-02-26 16:12:58 +0200

    add basic spring boot java application
    This include:
      database connectors for mysql and MS-sqlserver
      lombok for easy property generation in classes
      web package for rest calls
    
    Signed-off-by: Stefan Guelland <stefan@sguelland.de>

---
+++ src/main/java/na/org/ecb/complianceauditingportal/ComplianceAuditingPortalApplication.java
@@ -0,0 +1,12 @@
+package na.org.ecb.complianceauditingportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplianceAuditingPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplianceAuditingPortalApplication.class, args);
+	}
+}
